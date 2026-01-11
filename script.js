4e4d4425303b42daff847129b8518e564307587e
44mvtteo <moneyman20088@gmail.com>
2026-01-11 18:22:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,53 +21,23 @@ const FECHA_REENCUENTRO = new Date("2026-02-22T00:00:00-03:00");
  *
  *************************************************/
 const cartas = [
-  `Día 1
-
-Bueno mi amor este es el día uno de 45 días donde mi idea era ponerle la carta en la página y que la viera ahí pero como yo siempre supuse, la página se cayó.
-
-Me encantaría arreglarla pero ya me andan gritoniando para que me apure, lamento mucho que no esté funcionando pero le prometo que cuando llegue a la casa en España arreglaré todo lo posible y subiré cada carta que escribo al momento.
-
-Hoy día es 9 de enero, un día bastante triste para ser verano y estar soleado, aún ni siquiera salgo de mi casa pero cada vez su ausencia pesa más.
-
-Ninguno de los dos contaba con que discutiríamos pero pasó y solo me queda pedirle perdón al igual que usted a mí.
-
-De verdad quiero que lo nuestro funcione, hoy, mañana y por siempre.
-
-La amo mucho mi amor ❤️‍🩹
+  `Dia 1
+Bueno mi amor este es el dia uno de 45 dias donde mi idea era ponerle la carta en la pagina y que la viera ahi pero como yo siempre supuse, la pagina se cayó. Me encantaria arreglarla pero ya me andan gritoniando para que me apure, lamento mucho que no este funcionando pero le prometo que cuando llegue a la casa en españa arreglaré todo lo posible y subiré cada carta que escribo al momento. Hoy dia es 9 de enero, un dia bastante triste para ser verano y estan soleado, aun nisiquiera salgo de mi casa pero cada vez su ausencia pesa mas. Ninguno de los dos contaba con que discutiriamos pero paso y solo me queda pedirle perdon al igual que usted a mi. No tienen que ser asi las cosas, podemos estar bien, felices, amorosos, incluso si no nos podemos ver. 
+De verdad quiero que lo nuestro funcione, hoy, mañana y por siempre. La amo mucho mi amor espero que hoy sea un buen día en lo que se pueda y recuerde que en todo momento estaré pensandola. La amo muchísimo mi amor❤️‍🩹.
 
 9 de enero
-09:52`,
+09:52
+`,
 
-  `Día 2
-
-Holaa mi vida hoy es el día dos de 45.
-
-Le escribo esto mientras espero bajarme del avión porque ya llegué, siendo las 5:03 de la mañana hora Chile, pero acá en España son las 9 de la mañana.
-
-La extraño muchísimo, no sabe cuánto la he pensado.
-
-Su carta me emocionó mucho y funcionó lo que usted quería: lloré y también viví mi momento de película.
-
-No sabe la falta que me hace pero lo único que quiero es que estemos bien estos 45 días.
-
-La amo mucho amor mío ❤️‍🩹
+  `Dia 2
+Holaa mi vida hoy es el dia dos de 45, lee escribo esto mientras espero a bajarme del avión porque ya llegue siendo las 5:03 de la mañana hora chile peroo aca en españa son las 9 de la mañana. La extraño muchísimo no sabe cuanto la he pensado. Su carta me emociono mucho y funcionó lo que usted queria, llore y también vivi mi momento de película. No sabe la falta que me hace pero lo único que quiero es que estemos bien estos 45 dias. Lamentablemente no tengo como darle mantenimiento a la página asi que espero que cuando llegue a la casa encuentre la solución, ahora me esperan 5 horas de viaje en auto hacia sevilla y bueno quizas cuando usted lea esto yo ya estaré llegando. La amo mucho amor mio espero que este bien y espero que estemos bien, y tambien espero que me haya hablado mucho porque o si no me voy a enojarrr jejeje. La amo mucho mi amorcito 
+Por siempre❤️‍🩹
 
 10 de enero
 05:05`,
 
-  `Día 3
-
-Buen día wawita linda, o mejor dicho ya buenas tardes.
-
-Esta carta va con retraso porque tuve que salir y se me fue el tiempo.
-
-Hoy fui a comer a un lugar que se llama La Salá (nombre horrible), comí carne mechada con papas fritas y tomé La Casera de limón.
-
-Ahora estoy en la sala de música cantando horrible mientras usted duerme.
-
-La extraño muchísimo mi amor.
-
-Por siempre ♾️💘
+  `DIA 3
+Buen dia wawita linda o mejor dicho ya buenas tardes, esta carta va con retraso porque como tuve que salir se me fue el tiempo de las horas y como ya sabe no he podido arreglar la pagina pero creo que fue un error temporal. Hoy fui a comer a un lugar que se llama La salá pero no sala de estar salá de sal o salada KSKSKSKS nombres de mierdaa, y comi carne mechada con salsa de tomate y papas fritas y tome La casera de limón que es como la limón soda pero de españa. Ahora me encuentro en la sala de musica de mi prima cantando como si fuera experto mientras usted está tutito en la casa de esa weona. La extraño mucho amorcito mio ya quiero que llegue a su casita y podamos hacer llamada tranquilitosss jejeje. No sabe la falta que me hace y lo mucho que me gustaría que estuviera aqui conmigo mi amor. Eso pue espero que la haya pasado bien y que la siga pasando bien alla yo seguiré escuchando a mi prima tocar porque me duele la garganta de lo mal que canto, la amo mucho amorcito mio, por siempre♾️💘
 
 11 de enero
 12:59`,
@@ -266,6 +236,7 @@ function anterior() {
   indice = (indice - 1 + fotos.length) % fotos.length;
   mostrarFoto();
 }
+
 
 
 
