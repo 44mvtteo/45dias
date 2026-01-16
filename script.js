6d61a00f9072a4e6650791afbc1f6317e25889de
44mvtteo <moneyman20088@gmail.com>
2026-01-16 15:42:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -131,3 +131,172 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
 });
+
+const cartas = [
+`Dia 1
+Bueno mi amor este es el dia uno de 45 dias donde mi idea era ponerle la carta en la pagina y que la viera ahi pero como yo siempre supuse, la pagina se cayó. Me encantaria arreglarla pero ya me andan gritoniando para que me apure, lamento mucho que no este funcionando pero le prometo que cuando llegue a la casa en españa arreglaré todo lo posible y subiré cada carta que escribo al momento. Hoy dia es 9 de enero, un dia bastante triste para ser verano y estan soleado, aun nisiquiera salgo de mi casa pero cada vez su ausencia pesa mas. Ninguno de los dos contaba con que discutiriamos pero paso y solo me queda pedirle perdon al igual que usted a mi. No tienen que ser asi las cosas, podemos estar bien, felices, amorosos, incluso si no nos podemos ver. 
+De verdad quiero que lo nuestro funcione, hoy, mañana y por siempre. La amo mucho mi amor espero que hoy sea un buen día en lo que se pueda y recuerde que en todo momento estaré pensandola. La amo muchísimo mi amor❤️‍🩹.
+
+9 de enero
+09:52
+`,
+
+`Dia 2
+Holaa mi vida hoy es el dia dos de 45, lee escribo esto mientras espero a bajarme del avión porque ya llegue siendo las 5:03 de la mañana hora chile peroo aca en españa son las 9 de la mañana. La extraño muchísimo no sabe cuanto la he pensado. Su carta me emociono mucho y funcionó lo que usted queria, llore y también vivi mi momento de película. No sabe la falta que me hace pero lo único que quiero es que estemos bien estos 45 dias. Lamentablemente no tengo como darle mantenimiento a la página asi que espero que cuando llegue a la casa encuentre la solución, ahora me esperan 5 horas de viaje en auto hacia sevilla y bueno quizas cuando usted lea esto yo ya estaré llegando. La amo mucho amor mio espero que este bien y espero que estemos bien, y tambien espero que me haya hablado mucho porque o si no me voy a enojarrr jejeje. La amo mucho mi amorcito 
+Por siempre❤️‍🩹
+
+10 de enero
+05:05`,
+
+`DIA 3
+Buen dia wawita linda o mejor dicho ya buenas tardes, esta carta va con retraso porque como tuve que salir se me fue el tiempo de las horas y como ya sabe no he podido arreglar la pagina pero creo que fue un error temporal. Hoy fui a comer a un lugar que se llama La salá pero no sala de estar salá de sal o salada KSKSKSKS nombres de mierdaa, y comi carne mechada con salsa de tomate y papas fritas y tome La casera de limón que es como la limón soda pero de españa. Ahora me encuentro en la sala de musica de mi prima cantando como si fuera experto mientras usted está tutito en la casa de esa weona. La extraño mucho amorcito mio ya quiero que llegue a su casita y podamos hacer llamada tranquilitosss jejeje. No sabe la falta que me hace y lo mucho que me gustaría que estuviera aqui conmigo mi amor. Eso pue espero que la haya pasado bien y que la siga pasando bien alla yo seguiré escuchando a mi prima tocar porque me duele la garganta de lo mal que canto, la amo mucho amorcito mio, por siempre♾️💘
+
+11 de enero
+12:59`,
+
+`Dia 4
+Buen dia amorcito mio hoy es el dia cuatro de 45, me levante con ánimos y estoy listo para ir al gimnasio aunque todavía falta ya que es a las 1 y aca son recién las 11:40 pero mi tía tiene que hacer tramites así que la acompañare. No sabe el frío que tengo incluso con ropa deportiva, tampoco es que sea la mas abrigadora pero en Chile yo me cago de calor ocupando esto y aca me cago de frio. Hoy de desayuno tome pancito con queso y jamón y una leche con chocolate que quedo muy rica, nose que hare en la tarde ya que mi prima tienes clases de batería y nose que mas, yo habia propuesto ir al Lagoh, que es un mall muy grande con muchas tiendas es como un parque arauco o un alto las condes, ahi estan mis 4 tiendas favoritas 
+Bershka, Pull and Bear, Newyorker y Lefties. Quiero ir a verlas para que ver si tienen buena ropa en una de esas me gasto mi fortuna de 100 lukas ahi KSKSKSKSKS, aunque mi prima me dijo que en las tiendas de segunda mano tambien hay buena ropa, asi que espero también ir a ver las tiendas de segunda mano. Ya finalizando esta cartita espero que tenga un excelente dia donde podamos hablar harto y reírnos harto, apenas despierte y yo tenga Internet le hablare amorcito mio. La amo mucho mi wawi hemosha mucho beshitosss, 
+por siempree♾️💕
+
+12 de enero
+07:44 (hora Chile)
+`,
+
+`Dia 5
+Pensó que no habria carta hoy, bueno pues se equivoca, no me salto ningun dia y no tendra vacía ninguna cartita. Hoy mi amor es un dia muy especial ya que se cumplen 4 meses de que me dijo que si, un momento tan bonito y único que llevo cada dia de mi vida, incluso lo recuerdo como si hubiera sido ayer, ese dia fue tan bonito aunque empezo como las weas por culpa de sebita y sus mañas pero bueno. La pasamos tan bien despues de que se desenojo yo ya estaba estresandome ya pero gracias a dios salio como queria, llegamos a las dunas de concon (ojalá hubiera sido al atardecer mismo) y le pedi si podia ser su pololo. En el camino de vuelta me sentía tan feliz y tan completo, como nunca antes en la vida, sentí que todo mi esfuerzo valió la pena y asi llegamos hasta hoy, un buen dia aunque hayamos peleado, se que podemos arreglarnos y estar bien, ambos reaccionamos mal y bueno solo queda hablar las cosas y calmarnos aunque se que sigue enojada. Esto lo escribo mientras hago popo y hablo con usted pero aun no le diré porque claramente no es el momento, solo quiero que sepa que la amo mucho y que usted es mi absoluto todo. Agradezco cada segundo a su lado, agradezco poder hacerme llamar su pololo y agradezco poder decirle mi amor, y que usted me lo diga a mi es el sentimiento mas reconfortante del mundo. Tenemos problemas como cualquier relación pero se que nuestro amor es mas grande y podremos solucionar esto. La amo mucho amor mio, por siempre❤️‍🩹♾️
+13 de Enero
+14:26 `,
+
+`Dia 6
+Holaa mi amor hoy es el dia 6 de 45, me desperte muy contento, soñé con usted, soñé que estaba aca conmigo, que veíamos películas, que estábamos bien abrazaditos y que en una usted se subía encima mio, me encanta la tierno que somos y como siempre algo caliente tiene que pasar me gusta mucho esa conexión o esa confianza en ese sentido. Hoy nose que hare, me vengo recién levantando y aunque me hubiera gustado dormir en llamada con usted, su celu esta malo☹️. Me gusta que hablemos me gusta que nos demos atencion, creo que eso me mantiene cuerdo y aun no caigo en la locura, o quizas si?. Es bromaa, mis únicos síntomas de locura es el inmenso amor que le tengo y las cosas que haría por usted , usted me hace muy feliz sabe, me hace el weon mas feliz y no sabe como se lo agradezco, amo su amor, amo sus gestos, su atención, su confianza amo todo de usted y quiero amarlo por muchos años mas. La amo mucho amor mio me voy a tomar desayuno ya que mi tía fue a jugar papel a no ser que me vuelva a quedar dormido que es probable. La amo muchito wawi, por siempre♾️💕
+
+14 de enero
+6:44  `,
+
+`Dia 7
+Buenos días mi amor hoy es el dia 7 y siendo las 9 y cuarto vengo a escribirle la carta. Siento que estamos mejor pero aun asi faltan cosas por hablar, se que quiere arreglar las cosas y dejar de estar mal pero no sabe el miedo constante que tengo  dia a dia de que me termine, eso pienso cada dia y no quiero que sea asi, no quiero perderla y siempre elegiré el camino del diálogo con usted. Me hace mucha falta y solo quiero un abrazo de usted, solo quiero que arreglemos lo nuestro y hagamos todo lo que queríamos, perdón por mis malas actitudes, se que he reaccionado mal y dejaran de ser asi las cosas. Haría todo por usted eso téngalo claro y yo solo quiero cuidar lo nuestro porque la amo mucho y no quiero perderla. Se que a lo mejor estamos mejor que ayer en la noche pero el sueño me dejo muy mal sinceramente, y no puedo nisiquiera imaginar volver a estar sin usted. Arreglemos bien las cosas y en persona ya las arreglamos del todo, no nos perdamos y seamos felices mutuamente. La amo muchísimo mi amor hoy mañana
+ y siempre❤️‍🩹♾️
+
+15 de enero
+9:21 `,
+
+`Dia 8
+Buen dia amorcito mio, hoy es el dia 8 de 45, me encuentro en moron de la frontera, en la planta de explosivos Parraga, de Pepe Parraga o como usted lo conoce el millonario. Me dieron una vuelta por la fábrica y no entendi ni una mierda pero es muy bakaaan, han puesto mucho trabajo y mucho dinero y esta mucho mas grande que la primera vez que vine. El señor Parraga (como 70 años) me recibió super bien como si nos hubiéramos visto hace poco, me pregunto como estaban los estudios y mi familia y su hijo Miguel Angel (como 40 años) tambien me recibió como si me hubiera visto hace poco, me dijo: Mateo! Hombre, que cada vez estas mas grande, como estas. Esta siendo tan bacan venir para aca, quizas no entiendo y aun nose que me gustaría estudiar pero siento que cada vez estoy a un paso menos de descubrirlo. La extraño mucho mi amor, no sabe la falta que me hace pero si dios quiere algun dia estaremos tomados de la mano mirando hacia el futuro, con una linda casa, un lindo auto, unas lindas mascotas y unos lindos hijos, una hermosa familia. Ahora es la parte mas latera porque estan en una media reunión, no creo que demoren tanto pero igual me aburre, me gustaría estar alla dentro creyendo que se lo que hablan KSKSKSKSKS. Bueno mi amor hoy voy a arreglar la pagina que hice mierda por accidente asi que espero que pueda leer las cartas 5,6,7 y la de hoy 8. Espero que tenga un lindo día mi amorcito, la amo muchito hoy mañana y por siempre♾️💕.
+
+16 de enero
+08:43`,
+
+`Día 9
+Contenido de la carta 9.`,
+
+`Día 10
+Contenido de la carta 10.`,
+
+`Día 11
+Contenido de la carta 11.`,
+
+`Día 12
+Contenido de la carta 12.`,
+
+`Día 13
+Contenido de la carta 13.`,
+
+`Día 14
+Contenido de la carta 14.`,
+
+`Día 15
+Contenido de la carta 15.`,
+
+`Día 16
+Contenido de la carta 16.`,
+
+`Día 17
+Contenido de la carta 17.`,
+
+`Día 18
+Contenido de la carta 18.`,
+
+`Día 19
+Contenido de la carta 19.`,
+
+`Día 20
+Contenido de la carta 20.`,
+
+`Día 21
+Contenido de la carta 21.`,
+
+`Día 22
+Contenido de la carta 22.`,
+
+`Día 23
+Contenido de la carta 23.`,
+
+`Día 24
+Contenido de la carta 24.`,
+
+`Día 25
+Contenido de la carta 25.`,
+
+`Día 26
+Contenido de la carta 26.`,
+  `Día 27
+Contenido de la carta 27.`,
+
+`Día 28
+Contenido de la carta 28.`,
+
+`Día 29
+Contenido de la carta 29.`,
+
+`Día 30
+Contenido de la carta 30.`,
+
+`Día 31
+Contenido de la carta 31.`,
+
+`Día 32
+Contenido de la carta 32.`,
+
+`Día 33
+Contenido de la carta 33.`,
+
+`Día 34
+Contenido de la carta 34.`,
+
+`Día 35
+Contenido de la carta 35.`,
+
+`Día 36
+Contenido de la carta 36.`,
+
+`Día 37
+Contenido de la carta 37.`,
+
+`Día 38
+Contenido de la carta 38.`,
+
+`Día 39
+Contenido de la carta 39.`,
+
+`Día 40
+Contenido de la carta 40.`,
+
+`Día 41
+Contenido de la carta 41.`,
+
+`Día 42
+Contenido de la carta 42.`,
+
+`Día 43
+Contenido de la carta 43.`,
+
+`Día 44
+Contenido de la carta 44.`,
+
+`Día 45
+Esta es la última carta.
+El cierre del viaje.
+El reencuentro.
+El final que justifica todo el camino.`
+];
